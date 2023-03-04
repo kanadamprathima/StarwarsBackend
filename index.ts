819f4c84110b97a9ef3dd557f819c859dad725d2
prathima <kanadamprathima@gmail.com>
2023-03-04 22:45:21 +0100

    initialized express project prisma ORM

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = 4000;
+app.get("/", (req, res) => {
+  res.send("Hello ");
+});
+app.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
